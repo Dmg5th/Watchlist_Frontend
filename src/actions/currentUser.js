0533886328222f4de27437659d1030b3d41f00cb
Dmg5th <dmgould310@gmail.com>
2021-02-23 18:52:02 -0500

    Added a reducer and action creator for the login

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -5,9 +5,16 @@ export const setCurrentUser = user => {
         user
     }
 }
-//asychronous fetch request that returns an object because Thunk enables it to 
+//asychronous fetch request that returns an object because Thunk enables it to return an object as well as use dispatch
 export const logIn = credentials => {
     return dispatch => {
-        return fetch("http://localhost:3000/")
+        
+        return fetch("http://localhost:3001/login", {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({username: "Dm5th", password: "master"})
+        })
     }
-}
+} 
---
+++ src/reducers/loginForm.js
@@ -0,0 +1,15 @@
+const initialState = {
+    username: "",
+    password: ""
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        // case value:
+            
+        //     break;
+    
+        default:
+           return state
+    }
+}
