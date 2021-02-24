c0ad597e1e218900df75c372813b4e746c2fb116
Dmg5th <dmgould310@gmail.com>
2021-02-24 11:08:07 -0500

    Added mapStateToProps function into the login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Login = () => {
+const Login = (props) => {
     return  (
         <form onSubmit={undefined}>
             <input placeholder="username" value={undefined} name="username" type="text" onChange={undefined} ></input>
@@ -10,4 +11,17 @@ const Login = () => {
     )
 }
 
-export default Login
+const mapStateToProps = state => {
+    return {
+        username: state.loginForm.username,
+        password: state.loginForm.password
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps)(Login)
