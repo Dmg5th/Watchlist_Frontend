4ac53be882369c823a6bccdb8a577a2ca88db7c8
Dmg5th <dmgould310@gmail.com>
2021-02-25 13:39:25 -0500

    Added a MainContainer component

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContainer = () => {
+    return(
+        <div>
+            THIS IS THE MAIN CONTAINER
+        </div>
+    )
+}
+
+
+export default MainContainer; 
