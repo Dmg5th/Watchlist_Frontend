51dd35fc46a6d22038842b61a3236d924e699e29
Dmg5th <dmgould310@gmail.com>
2021-02-26 11:16:43 -0500

    Made a getMywatchlist action

--- src/actions/myWatchlist.js
+++ src/actions/myWatchlist.js
@@ -1,6 +1,31 @@
+//synchronous action 
 export const setCurrentUser = user => {
     return {
         type: 'SET_MY_WATCHLIST', 
         user
     }
+}
+
+//ascynchrounous action 
+export const getMyWatchlist = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/get_current_user", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json"
+            },
+          })
+            .then(r => r.json())
+            .then(response => {
+              if (response.error) {
+                alert(response.error)
+              } else {
+                dispatch()
+                // dispatch(getMyWatchlist())
+               }
+            })
+            .catch(console.log)
+    }
+  
 }
