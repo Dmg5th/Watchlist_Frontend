ea7164d15324f5ad57d0030e057baf663c1c7a6c
Dmg5th <dmgould310@gmail.com>
2021-02-26 12:41:42 -0500

    Made a MYWatchlist component

---
+++ src/components/MyWatchlist.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MyWatchlist extends React.Component {
+    render() {
+        return (
+            <div>
+                WATCHLIST
+            </div>
+        )
+    }
+}
+
+export default MyWatchlist 
