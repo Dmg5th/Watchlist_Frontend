197e93d4a0d5b6fb940f690fb35661ddeb257334
Dmg5th <dmgould310@gmail.com>
2021-03-01 16:21:38 -0500

    Made the watched functional component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 
 const NavBar = ({ currentUser, loggedIn }) => {
     return (
-        
+        <header>
         <div className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="#">WELCOME TO WATCHLIST!</a>
             { currentUser ? <strong> Welcome, {currentUser.attributes.username}!</strong>: ""}
@@ -16,6 +16,7 @@ const NavBar = ({ currentUser, loggedIn }) => {
                     <div className="collapse navbar-collapse"> <Link to="/signup" className="nav-link"> Signup</Link> OR <Link to="/login" className="nav-link">Log in</Link> </div>
                 )}
         </div>
+        </header>
     )
 }
 
