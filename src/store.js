f2248abcd4efe8e26d971f90dba2a5d6edcf3575
Dmg5th <dmgould310@gmail.com>
2021-03-01 10:11:04 -0500

    Maded signup form state

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import usersReducer from './reducers/users.js'
 import currentUser from './reducers/currentUser.js'
 import loginForm from './reducers/loginForm.js'
+import signupForm from './reducers/signupForm'
 import myWatchlist from './reducers/myWatchlist.js'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
@@ -10,7 +11,9 @@ const reducer = combineReducers({
     users: usersReducer, 
     currentUser,
     loginForm,
-    myWatchlist
+    signupForm, 
+    myWatchlist 
+
   })
   
   const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
