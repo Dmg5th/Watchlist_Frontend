12a8f7ef57db61a9e38e17f0bca4e10905d60647
Dmg5th <dmgould310@gmail.com>
2021-03-01 17:27:22 -0500

    Added routes for the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,29 +8,32 @@ const NavBar = ({ currentUser, loggedIn }) => {
     return (
 
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand" href="#">WELCOME TO WATCHLIST!</a>
+            <a className="navbar-brand"> <Link to="/">WELCOME TO WATCHLIST! </Link></a>
             {currentUser ? <strong> Welcome, {currentUser.attributes.username}!</strong> : ""}
             {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button> */}
-                 <div class="collapse navbar-collapse" id="navbarColor03">
+            <div class="collapse navbar-collapse" id="navbarColor03">
                 <ul class="navbar-nav mr-auto">
-                        {loggedIn ? <Logout />
-                            : (
-                                <div class="collapse navbar-collapse" id="navbarColor03">
+                    {loggedIn ? <Logout />
+                        : (
+                            <div class="collapse navbar-collapse" id="navbarColor03">
                                 <li class="nav-item active">
-                                    <a className="nav-link"><Link to="/signup" className="nav-link"> Signup </Link> </a> 
-                                    </li>
-                                    <li class="nav-item active">
-                                     <a ><Link to="/login" className="nav-link">Log in</Link> </a> 
+                                    <a className="nav-link"><Link to="/signup" className="nav-link"> Signup </Link> </a>
                                 </li>
-                                </div>
-                            )}
-                           <div class="collapse navbar-collapse" id="navbarColor03" >
-                            <li class="nav-item active">
-                            <a><Link to="/" className="nav-link">Watchlist</Link></a>
+                                <li class="nav-item active">
+                                    <a ><Link to="/login" className="nav-link">Log in</Link> </a>
+                                </li>
+                            </div>
+                        )}
+                    <div class="collapse navbar-collapse" id="navbarColor03" >
+                        <li class="nav-item active">
+                            <a><Link to="/user_movies" className="nav-link">Watchlist</Link></a>
                         </li>
-                        </div>
+                        <li class="nav-item active">
+                            <a><Link to="/watched" className="nav-link">Watched</Link></a>
+                        </li>
+                    </div>
                 </ul>
             </div>
         </nav>
