4d7c2b019e1ba63e96556eae5674e2bafc198ce7
Dmg5th <dmgould310@gmail.com>
2021-03-02 11:28:59 -0500

    Brought in the search api for the search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import Logout from './Logout'
 import { Link } from 'react-router-dom'
-// import Search from './components/movies/Search'
-import Search from '../components/movies/Search'
+import Search from './movies/Search'
 
 
 const NavBar = ({ currentUser, loggedIn }) => {
@@ -15,22 +14,22 @@ const NavBar = ({ currentUser, loggedIn }) => {
             {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button> */}
-            <div class="collapse navbar-collapse" id="navbarColor03">
-                <ul class="navbar-nav mr-auto">
-                    <div class="collapse navbar-collapse" id="navbarColor03" >
-                        <li class="nav-item active">
+            <div className="collapse navbar-collapse" id="navbarColor03">
+                <ul className="navbar-nav mr-auto">
+                    <div className="collapse navbar-collapse" id="navbarColor03" >
+                        <li className="nav-item active">
                             <a><Link to="/user_movies" className="nav-link">Watchlist</Link></a>
                         </li>
-                        <li class="nav-item active">
+                        <li className="nav-item active">
                             <a><Link to="/watched" className="nav-link">Watched</Link></a>
                         </li>
                         {loggedIn ? <Logout />
                         : (
-                            <div class="collapse navbar-collapse" id="navbarColor03">
-                                <li class="nav-item active">
+                            <div className="collapse navbar-collapse" id="navbarColor03">
+                                <li className="nav-item active">
                                     <a className="nav-link"><Link to="/signup" className="nav-link"> Signup </Link> </a>
                                 </li>
-                                <li class="nav-item active">
+                                <li className="nav-item active">
                                     <a ><Link to="/login" className="nav-link">Log in</Link> </a>
                                 </li>
                             </div>
--- src/components/movies/Search.js
+++ src/components/movies/Search.js
@@ -1,14 +1,38 @@
 import React from 'react'
 
-const Search = () => {
-    return (
-        <div>
-            <form class="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="text" placeholder="Search for a movie"/>
-              <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    )
+const featured_api = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&include_adult=false`
+
+class Search extends React.Component {
+    
+    state = {
+        query: ""
+     }
+
+    handleChange = e => {
+        // e.preventDefault();
+        this.setState({
+            query: e.target.value
+        }) 
+        fetch(featured_api)
+    }
+    render() {
+        console.log(this.state.query)
+        return (
+            <div>
+                <form class="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="text" placeholder="Search for a movie" value={this.state.query} onChange={this.handleChange} />
+                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Search; 
+
+
+
+
+
+
+
