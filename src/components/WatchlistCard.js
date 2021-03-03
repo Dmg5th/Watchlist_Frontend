c07c07f51eacf2220a536a0fe97cc7a3ef83bc0b
Dmg5th <dmgould310@gmail.com>
2021-03-03 11:44:29 -0500

    Made an onclick event for taking out a movie on the watchlist

--- src/components/WatchlistCard.js
+++ src/components/WatchlistCard.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Card }from 'react-bootstrap'
+import {connect} from 'react-redux'
+import { removeMovieFromWatchlist } from '../actions/myWatchlist'
 
-const WatchlistCard = ({movie}) => {
+const WatchlistCard = ({movie, removeMovieFromWatchlist}) => {
   
   return (
     <div className="main">
@@ -11,12 +13,15 @@ const WatchlistCard = ({movie}) => {
             <Card.Title className="card-header">{movie.title}</Card.Title>
             <div class="card-body">
               <Card.Img className="movie__image" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+              
               <button className="btn btn-success">
                 <i className="fa-fw far fa-eye"></i>
               </button>
-              <button className="btn btn-warning">
+              
+              <button onClick={() => removeMovieFromWatchlist(movie.id)} className="btn btn-warning">
                 <i className="fa-fw fa fa-times"></i>
               </button>
+            
             </div>
           </div>
         </div>
@@ -26,6 +31,10 @@ const WatchlistCard = ({movie}) => {
 }
 
 
+const mSTP = state => {
+  return {
+    watchlist: state.myWatchlist
+  }
+}
 
-
-export default WatchlistCard; 
+export default connect(mSTP, { removeMovieFromWatchlist })(WatchlistCard); 
