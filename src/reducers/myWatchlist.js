07cf5dde576632594db9cb64fcf9ee3fcb3a44e9
Dmg5th <dmgould310@gmail.com>
2021-03-03 12:28:50 -0500

    Fixed reducer not being iterable error

--- src/reducers/myWatchlist.js
+++ src/reducers/myWatchlist.js
@@ -10,19 +10,19 @@ export default (state = initialState, action) => {
     case "ADD_MOVIE_TO_WATCHLIST":
       return {
         ...state,
-        watchlist: [action.payload, ...state]
+        watchlist: [...state.watchlist, action.payload],
       }
     case "REMOVE_MOVIE_FROM_WATCHLIST":
       return {
         ...state,
-        watchlist: state.watchlist.filter(movieObj => movieObj.id !== action.payload)
+        watchlist: state.watchlist.filter(movieObj => movieObj.id !== action.payload),
       }
     case "ADD_MOVIE_TO_WATCHED":
       return {
         ...state,
-        watchlist: [action.payload, ...state]
-      }
-
+        watchlist: state.watchlist.filter(movieObj => movieObj.id !== action.payload.id),
+        watched: [...state.watched,action.payload]
+      };
     default:
       return state
   }
